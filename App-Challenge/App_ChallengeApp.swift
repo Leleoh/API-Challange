8f30309ae65f3fcf6dfa442c531c44f0665b1896
Leonel <leonelfhernandez@outlook.com>
2025-08-13 16:04:22 -0300

    Primeiro commit

---
+++ App-Challenge/App_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ChallengeApp.swift
+//  App-Challenge
+//
+//  Created by Leonel Ferraz Hernandez on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct App_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App-Challenge
+//
+//  Created by Leonel Ferraz Hernandez on 13/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
