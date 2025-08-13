750bd787abbe28b7063354045ec9291bff2bd124
Leonel <leonelfhernandez@outlook.com>
2025-08-13 16:24:49 -0300

    Teste

---
+++ App-Challenge/Components/Component1.swift
@@ -0,0 +1,18 @@
+//
+//  Component1.swift
+//  App-Challenge
+//
+//  Created by Leonel Ferraz Hernandez on 13/08/25.
+//
+
+import SwiftUI
+
+struct Component1: View {
+    var body: some View {
+        Text("Hello, World!!")
+    }
+}
+
+#Preview {
+    Component1()
+}
