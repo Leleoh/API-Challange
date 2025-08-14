763a54f73bb59f0307eb51109dd1482dfdae0c98
Leonel <leonelfhernandez@outlook.com>
2025-08-14 16:27:13 -0300

    Componente de foto do produto em detalhes

---
+++ App-Challenge/Components/ProductCards/ProductPhotoLarge.swift
@@ -0,0 +1,36 @@
+//
+//  ProductPhotoLarge.swift
+//  App-Challenge
+//
+//  Created by Leonel Ferraz Hernandez on 14/08/25.
+//
+
+import SwiftUI
+
+struct ProductPhotoLarge: View {
+    
+    let heart = FavoriteIcon()
+    
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 16)
+                .foregroundStyle(.backgroundsSecondary)
+                .frame(width: 361, height: 361)
+            
+            VStack {
+                ZStack(alignment: .topTrailing) {
+                    Image("PlaceholderMediumCard")
+                        .resizable()
+                        .frame(width: 329, height: 329)
+                        .cornerRadius(8)
+                        
+                    heart
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProductPhotoLarge()
+}
